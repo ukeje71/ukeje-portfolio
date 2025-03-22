390cad72a848776b51d30705380d83ee7d69c339
ukeje isaac <ukejeisaac71@gmail.com>
2025-03-22 08:37:51 +0630

    feature added imgage into an img folder and completed contact section for mobile devices

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+prompt("PLEASE INPUT USER NAME")
+
+
 document.addEventListener("DOMContentLoaded",()=>{
     const menuBar =document.querySelector(".menu-bar");
     const nav =document.querySelector("nav"); 
@@ -7,3 +10,21 @@ document.addEventListener("DOMContentLoaded",()=>{
         nav.classList.toggle("menu-open");
     })
 })
+
+const svg =document.querySelector(".svg");
+const BRE =document.querySelector(".BRE");
+const formBtn =document.querySelector(".formBtn");
+const head =document.querySelector(".hd-bg");
+
+svg.addEventListener("click",()=>{
+alert("YOU WILL BE REDIRECTED")
+});
+BRE.addEventListener("click",()=>{
+    alert("YOU WILL BE REDIRECTED FOR MORE TECH BERRIES BEFORE THAT DO YOU KNOW THAT THERE ARE FACT BEHIND SOME TECH RUMORS")
+    });
+    formBtn.addEventListener("click",()=>{
+        alert("THANK YOU FOR YOUR RESPONSE YOUR FEEDBACK MATTERS FOR OUR IMPROVEMENT")
+        });
+        // head.addEventListener("onmouseleave",()=>{
+        //     prompt("PLEASE INPUT USER NAME")
+        //     });
